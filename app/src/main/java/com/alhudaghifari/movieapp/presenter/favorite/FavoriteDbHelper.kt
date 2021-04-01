e45d9ca5867d2719f34aa3e53a6b77017e4e6c06
alhudaghifari <alhudaghifari@gmail.com>
2021-04-02 01:07:38 +0800

    drawable fav red added

--- app/src/main/java/com/alhudaghifari/movieapp/presenter/favorite/FavoriteDbHelper.kt
+++ app/src/main/java/com/alhudaghifari/movieapp/presenter/favorite/FavoriteDbHelper.kt
@@ -12,12 +12,9 @@ import kotlinx.coroutines.launch
 class FavoriteDbPresenter(private val dbHelper: DatabaseHelper) :
     ViewModel() {
     private val movies = MutableLiveData<Resource<List<Movie>>>()
+    private val singleMovie = MutableLiveData<Resource<List<Movie>>>()
 
-    init {
-        fetchMovies()
-    }
-
-    private fun fetchMovies() {
+    fun fetchMovies() {
         viewModelScope.launch {
             movies.postValue(Resource.loading(null))
             try {
@@ -27,14 +24,33 @@ class FavoriteDbPresenter(private val dbHelper: DatabaseHelper) :
                 } else {
                     movies.postValue(Resource.success(listOf()))
                 }
-
             } catch (e: Exception) {
                 movies.postValue(Resource.error("Something went wrong", null))
             }
         }
     }
 
+    fun fetchMovieById(idMovie: String) {
+        viewModelScope.launch {
+            singleMovie.postValue(Resource.loading(null))
+            try {
+                val moviesFromDb = dbHelper.getMovieById(idMovie)
+                if (moviesFromDb.isNotEmpty()) {
+                    singleMovie.postValue(Resource.success(moviesFromDb))
+                } else {
+                    singleMovie.postValue(Resource.success(listOf()))
+                }
+            } catch (e: Exception) {
+                singleMovie.postValue(Resource.error("Something went wrong", null))
+            }
+        }
+    }
+
     fun getMovies(): LiveData<Resource<List<Movie>>> {
         return movies
+    }
+
+    fun getSingleMovie(): LiveData<Resource<List<Movie>>> {
+        return singleMovie
     }
 }
